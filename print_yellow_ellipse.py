b215bfe4332c00025af5778ead0f34bf47d10ee2
Kirill Nefedov <knefyodov@bk.ru>
2021-11-20 17:25:19 +0300

    Main First Commit

---
+++ print_yellow_ellipse.py
@@ -0,0 +1,36 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QColor, QPainter
+from PyQt5 import uic
+import sys
+import random
+
+
+class MainP(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_ellipse(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_ellipse(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        x = random.randrange(10, 200, 10)
+        qp.drawEllipse(random.randrange(10, 400, 10), random.randrange(10, 400, 10), x, x)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainP()
+    ex.show()
+    sys.exit(app.exec())
